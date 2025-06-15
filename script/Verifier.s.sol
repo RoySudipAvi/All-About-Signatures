c502a28942e50ffcfd6d95805d3be90edbe281de
Sudip Roy <roysudipwork@proton.me>
2025-06-15 16:47:25 +0530

    updated the entire code (#1)

--- script/Verifier.s.sol
+++ script/Verifier.s.sol
@@ -3,12 +3,15 @@ pragma solidity 0.8.26;
 
 import {Script} from "forge-std/Script.sol";
 import {Verifier} from "src/Verifier.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract VeriferDeploy is Script {
     Verifier private s_verifier;
     function run() external returns (Verifier) {
-        vm.startBroadcast();
-        s_verifier = new Verifier("EIP-191 Verification", "1.0");
+        HelperConfig helper = new HelperConfig();
+        HelperConfig.Config memory config = helper.getConfigByChainId();
+        vm.startBroadcast(config.deployer);
+        s_verifier = new Verifier("Demo Signatures", "1.0");
         vm.stopBroadcast();
         return s_verifier;
     }
--- test/Verifier.t.sol
+++ test/Verifier.t.sol
@@ -2,29 +2,180 @@
 pragma solidity 0.8.26;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Verifier} from "src/Verifier.sol";
-import {VeriferDeploy} from "script/Verifier.s.sol";
+import {Verifier, InvalidSignature, InvalidSignatureLength} from "src/Verifier.sol";
+import {Vm} from "forge-std/Vm.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract VerifierTest is Test {
-    //using MessageHashUtils for bytes32;
-    Verifier private s_verfier;
-    address private s_address;
-    uint256 private s_privateKey;
+    using MessageHashUtils for bytes32;
+
+    uint256 private constant PRIVATE_KEY =
+        0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
+    Verifier private s_signatures;
+    address private s_signer;
+    Vm.Wallet private s_aliceWallet;
+
+    uint256 private constant HIGH_S =
+        0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0 + 1;
+
     function setUp() external {
-        VeriferDeploy _deploy = new VeriferDeploy();
-        s_verfier = _deploy.run();
-        (s_address, s_privateKey) = makeAddrAndKey("alice");
+        s_signatures = new Verifier("Demo Signatures", "1.0");
+        s_signer = vm.addr(PRIVATE_KEY);
+        s_aliceWallet = vm.createWallet("alice");
     }
 
-    function testPersonalSignVerification() external {
-        string memory _message = "Hello!!! This is Alice";
-        vm.startPrank(s_address);
-        bytes32 hash = keccak256(abi.encodePacked(_message));
-        (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(s_privateKey, hash);
-        vm.stopPrank();
-        address _signer = s_verfier.getSigner(_message, _v, _r, _s);
-        console.log(s_address);
-        console.log(_signer);
+    // function testDumps() external view {
+    //     console.log(s_signer); // Signer address
+    //     console.log(s_aliceWallet.addr); // Another Signer Address
+    //     console.logString(s_signatures.uintToString(45)); // uint to string test
+    //     console.logBytes(s_signatures.prefixData(bytes1(0x00), "Lorem Ipsum DOler SIt AMut")); // prefix test for data for validator
+    //     console.logBytes(s_signatures.prefixData(bytes1(0x45), "Lorem Ipsum DOler SIt AMut")); // prefix test gfor personal signing
+    // }
+
+    function _makeHighSBytesSig(
+        bytes32 digest
+    ) internal returns (bytes memory) {
+        // create a valid signature first
+        (uint8 v, bytes32 r, ) = vm.sign(PRIVATE_KEY, digest);
+        // force s to be too large
+        bytes32 badS = bytes32(HIGH_S);
+        return abi.encodePacked(r, badS, v);
+    }
+
+    function testPersonalSignRevertOnHighS() external {
+        string memory msg_ = "Personal test";
+        bytes32 digest = MessageHashUtils.toEthSignedMessageHash(bytes(msg_));
+        bytes memory badSig = _makeHighSBytesSig(digest);
+
+        vm.expectRevert(InvalidSignature.selector);
+        s_signatures.personalMessageSign(msg_, badSig);
+    }
+
+    function testPersonalSignRevertForInvalidSignatureLength() external {
+        bytes memory invalidSig = new bytes(64);
+        string memory msg_ = "Personal test";
+        vm.expectRevert(InvalidSignatureLength.selector);
+        s_signatures.personalMessageSign(msg_, invalidSig);
+    }
+
+    function testPersonalSignMutatedMessage() external {
+        string memory _message = "Ethereum is based";
+        bytes memory _messageBytes = bytes(_message);
+        bytes32 _digest = MessageHashUtils.toEthSignedMessageHash(
+            _messageBytes
+        );
+        (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(s_aliceWallet, _digest);
+        bytes memory _signature = abi.encodePacked(_r, _s, _v);
+        string memory _messageWrong = "Ethereum is baaad";
+        address _signerReturnedWrong = s_signatures.personalMessageSign(
+            _messageWrong,
+            _signature
+        );
+
+        address _signerReturned = s_signatures.personalMessageSign(
+            _message,
+            _signature
+        );
+
+        assertNotEq(_signerReturned, _signerReturnedWrong);
+    }
+
+    function testSignForValidators() external {
+        string memory _message = "Ethereum is based";
+        bytes memory _messageBytes = bytes(_message);
+        bytes32 _digest = MessageHashUtils.toDataWithIntendedValidatorHash(
+            address(s_signatures),
+            _messageBytes
+        );
+        (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(PRIVATE_KEY, _digest);
+        bytes memory _signature = abi.encodePacked(_r, _s, _v);
+        address _signerReturned = s_signatures.getSignerForValidator(
+            _message,
+            _signature
+        );
+        assertEq(s_signer, _signerReturned);
+    }
+
+    function testPersonalSign() external {
+        string memory _message = "Ethereum is based";
+        bytes memory _messageBytes = bytes(_message);
+        bytes32 _digest = MessageHashUtils.toEthSignedMessageHash(
+            _messageBytes
+        );
+        (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(s_aliceWallet, _digest);
+        bytes memory _signature = abi.encodePacked(_r, _s, _v);
+        address _singerReturned = s_signatures.personalMessageSign(
+            _message,
+            _signature
+        );
+        assertEq(s_aliceWallet.addr, _singerReturned);
+    }
+
+    function testStructuredDataSign() external {
+        string memory _message = "Signing EIP-712 data";
+        bytes32 _hashStructMessage = keccak256(
+            abi.encode(
+                s_signatures.MESSAGE_TYPE_HASH(),
+                keccak256(bytes(_message)),
+                s_signer
+            )
+        );
+        bytes32 _digest = MessageHashUtils.toTypedDataHash(
+            s_signatures.I_DOMAIN_SEPERATOR(),
+            _hashStructMessage
+        );
+        (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(PRIVATE_KEY, _digest);
+        bytes memory _signature = abi.encodePacked(_r, _s, _v);
+        Verifier.Message memory _Message = Verifier.Message({
+            message: _message,
+            sender: s_signer
+        });
+        address _signerReturned = s_signatures.getSignerStructuredData(
+            _Message,
+            _signature
+        );
+        assertEq(s_signer, _signerReturned);
+    }
+
+    function testMultipleStructuredDataSign() external {
+        Verifier.Branch memory _branchFrom = Verifier.Branch({
+            id: 3,
+            name: "Audit"
+        });
+
+        Verifier.Person memory _personFrom = Verifier.Person({
+            name: "Chotu",
+            wallet: 0x70997970C51812dc3A010C7d01b50e0d17dc79C8,
+            branch: _branchFrom
+        });
+
+        Verifier.Branch memory _branchTo = Verifier.Branch({
+            id: 2,
+            name: "Web3 Development"
+        });
+
+        Verifier.Person memory _personTo = Verifier.Person({
+            name: "Golu",
+            wallet: 0x976EA74026E726554dB657fA54763abd0C3a0aa9,
+            branch: _branchTo
+        });
+
+        Verifier.Mail memory _mail = Verifier.Mail({
+            from: _personFrom,
+            to: _personTo,
+            subject: "Issue regarding reentrancy",
+            content: "In the withdraw function there is an issue of reentrancy, make sure to follow CEI pattern"
+        });
+
+        bytes32 _hashStructMail = s_signatures.hashStructMail(_mail);
+        bytes32 _digest = MessageHashUtils.toTypedDataHash(
+            s_signatures.I_DOMAIN_SEPERATOR(),
+            _hashStructMail
+        );
+        (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(s_aliceWallet, _digest);
+        bytes memory _signature = abi.encodePacked(_r, _s, _v);
+        address _signerReturned = s_signatures
+            .getSignerStructuredDataMultipleStructs(_mail, _signature);
+        assertEq(s_aliceWallet.addr, _signerReturned);
     }
 }
